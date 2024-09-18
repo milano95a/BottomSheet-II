c055869766649bad0d30ed4b1abcdcb65c9c52fa
Jamoliddinov Abduraxmon <abduraxmon.jamoliddinov@smartbank.uz>
2024-09-18 16:21:42 +0500

    Initial Commit

---
+++ BottomSheet-II/BottomSheet_IIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BottomSheet_IIApp.swift
+//  BottomSheet-II
+//
+//  Created by Jamoliddinov Abduraxmon on 18/09/24.
+//
+
+import SwiftUI
+
+@main
+struct BottomSheet_IIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
